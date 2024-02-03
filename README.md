6ce186fefc79b10b64a7e5f665f21d36d4b5f358
Valinohtar <pmusial13@gmail.com>
2024-02-03 09:35:26 +0100

    Final version

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # timed_math_challenges
+
+
+## Rules
+
+You choose how many levels there are. Each level consists of 5 questions. You have 3 lives per 1 level.
+App measures time to finish and percentage of the correct answers.
+
+## Funcionality
+
+App allows to save your score as txt file. You can choose how many level you want to play
+
+### Things to change
+
+* Add possibility for user to change gow many levels they are - done
+* Add text for losing the game beforehand - done
+* Correct the percentage - done
--- times_math_challenge.py
+++ times_math_challenge.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
@@ -14,30 +15,60 @@ def generate_problem(level):
     return expr, answer
 
 points = 0
+problem_num = 1
+lifes = 3
+bad = 0
 
-for i in range(1,4):    
-    lifes = 3
-    if lifes < 0:
-        break
+levels = input("How many levels do you want to play?")
+
+if levels.isdigit() == False:
+    print("Please, give a number!\n")
+    levels = input("How many level do you want to play?")
+
+
+input("Press any KEY to start!")
+
+start_time = time.time()
+
+for i in range(1,int(levels)+1):    
+    count = 0
+    if lifes <= 0:
         print("You have lost.")
-    print(f"Level {i}!\n")
-    while points < 3:
+        break
+        
+    lifes = 3
+    
+    print(f"Level {i}!\nYou have {lifes} lives!")
+    while count < 5:
         expr, answer = generate_problem(i)
-        print("Give and answer!")
+        print(f"Problem #{problem_num}. Give an answer!")
+        problem_num += 1
         print(expr)
-        user_answer = int(input())
-        if user_answer == answer:
+        user_answer = input()
+        count += 1
+        if user_answer == str(answer):
             points += 1
             print("Congratulations. You points: ", points)
         else:
             lifes -=1
+            bad += 1
             print("Bad. Your lives:", lifes)
 
     
+end_time = time.time()
+total_time = end_time - start_time
 
-
-
-
+if lifes > 0:
+    score = f"It took you {round(total_time, 2)} second to finish the challenge with {levels} levels. Your score is {round(points/(points+bad), 5)*100}%."
+    print(score)
+    print_score = input("Do you want to safe your score? Press Y for yes.")
+    if print_score.lower() == "y":
+        name = input("Please share your name: ")
+        with open(f'score-{name}.txt', 'w') as f:
+            f.write(f'Name: {name} | Levels: {levels} | Time: {round(total_time, 2)} | Score: {round(points/(points+bad), 4)*100}%')
+    
+else:
+    print("You didn't finish the challenge!")
 
 
 
